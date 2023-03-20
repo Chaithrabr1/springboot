b63e17fb70a78115d1a5ae99e69c0aab4f796340
chaithra <chaithrabr586@gmail.com>
2023-03-20 17:41:02 +0530

    first commit

---
+++ src/main/java/com/springboot/application/repository/EmployeeRespository.java
@@ -0,0 +1,10 @@
+package com.springboot.application.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springboot.application.model.Employee;
+@Repository
+public interface EmployeeRespository  extends JpaRepository<Employee, Long>{
+
+}
